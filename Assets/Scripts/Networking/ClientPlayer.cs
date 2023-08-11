bb2019cc2f03750a9a2d158b18b73cd94e21cbce
Sayna <nwott1@gmail.com>
2023-08-11 14:05:28 -0400

    Player health might work
    - Need more to be done to test it

--- Assets/Scripts/Networking/ClientPlayer.cs
+++ Assets/Scripts/Networking/ClientPlayer.cs
@@ -8,6 +8,10 @@ using FishNet.Connection;
 public class ClientPlayer : NetworkBehaviour
 {
     [SyncVar] private string username;
+    [SyncVar] private int health;
+
+    private float currentTime; // amount of time in seconds that the player has been alive for
+    private float longestTime; // amount of time in seconds that the player has been alive for the longest
 
     public string Username { get { return username; } }
 
@@ -21,6 +25,7 @@ public class ClientPlayer : NetworkBehaviour
         }
     }
 
+    // what happens when the player joins the server
     [ServerRpc]
     private void Initialize()
     {
@@ -37,5 +42,11 @@ public class ClientPlayer : NetworkBehaviour
         if (IsServer) return;
 
         Debug.Log(message);
+    }
+
+    [ServerRpc]
+    public void ChangeHealth(int change)
+    {
+        health += change;
     }
 }
