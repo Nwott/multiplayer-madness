c8d402a07fac616a6ce3d8af3df58a89a81559e8
Sayna <nwott1@gmail.com>
2023-08-12 12:02:26 -0400

    Item scriptable object

---
+++ Assets/Scripts/Items/ItemScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemScriptableObject", menuName = "Sayna/Item")]
+public class ItemScriptableObject : ScriptableObject
+{
+    public string itemName;
+    [TextArea] public string description;
+    public bool throwable;
+}
